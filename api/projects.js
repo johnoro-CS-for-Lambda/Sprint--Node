f09108f044102de75c287829e0cb953f95413f73
johnoro <officialjohnorourke@gmail.com>
2018-08-31 11:47:47 -0400

    Completed basic CRUD functionality for projects.

--- api/projects.js
+++ api/projects.js
@@ -21,4 +21,40 @@ router.get('/:id', (req, res) => {
     });
 });
 
+router.get('/:id/actions', (req, res) => {
+  db.getProjectActions(req.params.id)
+    .then(actions => res.status(200).json(actions))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Server error getting the actions for that project.' });
+    });
+});
+
+router.post('/', (req, res) => {
+  db.insert(req.body)
+    .then(() => getProjects(req, res))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Server error adding the project.' });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  db.update(req.params.id, req.body)
+    .then(() => getProjects(req, res))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Server error updating that project.' });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  db.remove(req.params.id)
+    .then(() => getProjects(req, res))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Server error deleting that project.' });
+    });
+});
+
 module.exports = router;
