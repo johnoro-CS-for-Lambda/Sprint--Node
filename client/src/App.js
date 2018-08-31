6eacd8b1edf16c90271451ffec1cbf465c1f97ea
johnoro <officialjohnorourke@gmail.com>
2018-08-31 13:58:12 -0400

    Added routing to each project's actions, and also styled slightly more.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route, Link } from 'react-router-dom';
 import { Icon } from 'react-fa';
 
 const URL = 'http://localhost:8000/api/projects';
@@ -13,26 +14,84 @@ class App extends Component {
       .catch(err => console.error(err));
   }
 
+  getProjectActions = id => {
+    axios.get(`${URL}/${id}/actions`)
+      .then(({ data }) => {
+        this.setState({ actions: data, id: this.state.projects.find(project => project.id === id).id})
+      })
+      .catch(err => console.error(err));
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Node Express Projects</h1>
         </header>
-        <div className="projects">
-          {this.state.projects.map(project => {
-            const { id, name, description, completed } = project;
-            return (
-              <div className="project" key={id}>
-                <h3>{name}</h3>
-                <span>Status: {completed ? 
-                  <span>Completed <Icon name="check" /></span> : 
-                  <span>Not completed <Icon name="times" /></span>}</span>
-                <p>{description}</p>
+        <Route exact path="/" render={() => {
+          return (
+            <div className="projects">
+              {this.state.projects.map(project => {
+                const { id, name, description, completed } = project;
+                return (
+                  <div className="project" key={id}>
+                    <h3>{name}</h3>
+                    <span>Status: {completed 
+                      ? <span>Completed <Icon name="check" /></span> 
+                      : <span>Not completed <Icon name="times" /></span>}
+                    </span>
+                    <p>{description}</p>
+                    <Link 
+                      to={`/${id}`} 
+                      onClick={() => this.getProjectActions(id)}
+                    >
+                      More details >
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        }} />
+          
+        <Route exact path="/:id" render={() => {
+          return (
+            <div>
+              {this.state.projects
+                .filter(project => project.id === this.state.id)
+                .map(project => {
+                  const { id, name, description, completed } = project;
+                  return (
+                    <div className="project" key={id}>
+                      <h2>{name}</h2>
+                      <span>Status: {completed 
+                        ? <span>Completed <Icon name="check" /></span> 
+                        : <span>Not completed <Icon name="times" /></span>}
+                      </span>
+                      <p>{description}</p>
+                    </div>
+                  );
+                })
+              }
+              <div className="actions">
+                <h3>Actions:</h3>
+                {this.state.actions.map(action => {
+                  const { id, description, notes, completed } = action;
+                  return (
+                    <div className="action" key={id}>
+                      <h4>{description}</h4>
+                      <p>{notes}</p>
+                      <span>Status: {completed 
+                        ? <span>Completed <Icon name="check" /></span> 
+                        : <span>Not completed <Icon name="times" /></span>}
+                      </span>
+                    </div>
+                  );
+                })}
               </div>
-            );
-          })}
-        </div>
+            </div>
+          );
+        }} />
       </div>
     );
   }
